314e18d397ff6ecc41fd29c46f08f215a6e13644
Mr-Bucky <mbaklarz@yahoo.com>
2026-08-17 19:21:09 -0700

    Delete generate.sh
    Placed into folder tree.

--- generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /tmp
-
-curl -s https://bsideslv.org/talks -o bsideslv.talks
-grep -A 3 h4 bsideslv.talks | sed -e 's/    <h4.*\">/"/g' -e 's/<\/h4>/",/g' -e 's/&amp;/\&/g' -e 's/    <p>//g' -e 's/<\/p>//g' -e 's/\-\-//g' -e 's/, Monday/,Monday,/g' -e 's/, Tuesday/,Tuesday,/g' -e 's/, Wednesday/,Wednesday,/g' | grep . | paste -d' ' - - | sort > BSidesLV_2026_Sort.csv
-
-exit
